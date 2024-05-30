6e571ef7222c22bcfc3445a149d27b038d89748f
Diko Cesartista <dikocesrt@gmail.com>
2024-05-30 09:19:44 +0700

    finished get liked story by user id

--- controllers/story/story.go
+++ controllers/story/story.go
@@ -84,3 +84,38 @@ func (storyController *StoryController) GetStoryById(c echo.Context) error {
 
 	return c.JSON(http.StatusOK, base.NewSuccessResponse("Success Get Story By Id", storyResp))
 }
+
+func (storyController *StoryController) GetLikedStories(c echo.Context) error {
+	pageParam := c.QueryParam("page")
+	limitParam := c.QueryParam("limit")
+
+	metadata := utilities.GetMetadata(pageParam, limitParam)
+
+	token := c.Request().Header.Get("Authorization")
+	userId, _ := utilities.GetUserIdFromToken(token)
+
+	stories, err := storyController.storyUseCase.GetLikedStories(*metadata, userId)
+	if err != nil {
+		return c.JSON(base.ConvertResponseCode(err), base.NewErrorResponse(err.Error()))
+	}
+
+	storiesResp := make([]response.StoriesGetAllResponse, len(stories))
+
+	for i, story := range stories {
+		storiesResp[i] = response.StoriesGetAllResponse{
+			ID:       story.Id,
+			Title:    story.Title,
+			Content:  story.Content,
+			Date:     story.Date,
+			ImageUrl: story.ImageUrl,
+			ViewCount: story.ViewCount,
+			IsLiked:  story.IsLiked,
+			Doctor: response.DoctorGetAllResponse{
+				ID:   story.Doctor.ID,
+				Name: story.Doctor.Name,
+			},
+		}
+	}
+
+	return c.JSON(http.StatusOK, base.NewMetadataSuccessResponse("Success Get Liked Stories", metadata, storiesResp))
+}
