22281011dad82d1e361717770e928d34e12d1016
Diko Cesartista <dikocesrt@gmail.com>
2024-06-22 08:53:59 +0700

    fixed change email feature

--- controllers/otp/otp.go
+++ controllers/otp/otp.go
@@ -3,6 +3,7 @@ package otp
 import (
 	"capstone/controllers/otp/request"
 	otpEntities "capstone/entities/otp"
+	"capstone/utilities"
 	"capstone/utilities/base"
 	"net/http"
 
@@ -31,6 +32,25 @@ func (otpController *OtpController) SendOtp(c echo.Context) error {
 	}
 
 	err = otpController.otpUseCase.SendOTP(otpEnt)
+	if err != nil {
+		return c.JSON(base.ConvertResponseCode(err), base.NewErrorResponse(err.Error()))
+	}
+
+	return c.JSON(http.StatusCreated, base.NewSuccessResponse("Success send otp", nil))
+}
+
+func (otpController *OtpController) SendOTPChangeEmail(c echo.Context) error {
+	var req request.OTPRequest
+	err := c.Bind(&req)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, base.NewErrorResponse(err.Error()))
+	}
+
+	otpEnt := otpEntities.Otp{
+		Email: req.Email,
+	}
+
+	err = otpController.otpUseCase.SendOTPChangeEmail(otpEnt)
 	if err != nil {
 		return c.JSON(base.ConvertResponseCode(err), base.NewErrorResponse(err.Error()))
 	}
@@ -85,15 +105,21 @@ func (otpController *OtpController) VerifyOTPChangeEmail(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, base.NewErrorResponse(err.Error()))
 	}
 
+	token := c.Request().Header.Get("Authorization")
+	userId, err := utilities.GetUserIdFromToken(token)
+	if err != nil {
+		return c.JSON(http.StatusUnauthorized, base.NewErrorResponse("Invalid token"))
+	}
+
 	otpEnt := otpEntities.Otp{
 		Email: req.Email,
 		Code:  req.Code,
 	}
 
-	err = otpController.otpUseCase.VerifyOTPChangeEmail(otpEnt)
+	err = otpController.otpUseCase.VerifyOTPChangeEmail(userId, otpEnt)
 	if err != nil {
 		return c.JSON(base.ConvertResponseCode(err), base.NewErrorResponse(err.Error()))
 	}
 
-	return c.JSON(http.StatusOK, base.NewSuccessResponse("Success verify otp", nil))
+	return c.JSON(http.StatusOK, base.NewSuccessResponse("Success change email", nil))
 }
--- usecases/otp/otp.go
+++ usecases/otp/otp.go
@@ -40,6 +40,30 @@ func (otpUseCase *OtpUseCase) SendOTP(otp otpEntities.Otp) error {
 	return nil
 }
 
+func (otpUseCase *OtpUseCase) SendOTPChangeEmail(otp otpEntities.Otp) error {
+	if otp.Email == "" {
+		return constants.ErrEmptyInputEmailOTP
+	}
+
+	codeOTP := utilities.GenerateOTP()
+	expiry := utilities.GenerateExpiryTime()
+
+	otp.Code = codeOTP
+	otp.ExpiredAt = expiry
+
+	err := otpUseCase.otpInterface.SendOTPChangeEmail(otp)
+	if err != nil {
+		return err
+	}
+
+	err = utilities.SendEmail(otp.Email, codeOTP)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (otpUseCase *OtpUseCase) VerifyOTP(otp otpEntities.Otp) error {
 	if otp.Email == "" || otp.Code == "" {
 		return constants.ErrEmptyInputVerifyOTP
@@ -66,12 +90,12 @@ func (otpUseCase *OtpUseCase) VerifyOTPRegister(otp otpEntities.Otp) error {
 	return nil
 }
 
-func (otpUseCase *OtpUseCase) VerifyOTPChangeEmail(otp otpEntities.Otp) error {
+func (otpUseCase *OtpUseCase) VerifyOTPChangeEmail(userId int, otp otpEntities.Otp) error {
 	if otp.Email == "" || otp.Code == "" {
 		return constants.ErrEmptyInputVerifyOTP
 	}
 
-	err := otpUseCase.otpInterface.VerifyOTPChangeEmail(otp)
+	err := otpUseCase.otpInterface.VerifyOTPChangeEmail(userId, otp)
 	if err != nil {
 		return err
 	}
